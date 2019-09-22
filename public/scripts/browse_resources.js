9e30202a38e0bc4cb733b3a88cc5deceaff9a1b6
Jonathan Milne-Skarzenski <j.milne21@gmail.com>
2019-09-22 17:09:47 +0000

    Commiting all minor changes so I can try to pull

--- public/scripts/browse_resources.js
+++ public/scripts/browse_resources.js
@@ -1,19 +1,3 @@
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
-  host: 'localhost',
-  database: 'lhlmidterm'
-});
-
-module.exports = {
-  query: (text, params, callback) => {
-    return pool.query(text, params, callback)
-  },
-}
-
-
 const createResource = data => {
 `<h3>${findAllResources(data).title}</h3>
   <div>
@@ -23,27 +7,24 @@ const createResource = data => {
   </div>`
 }
 
-
 const renderResource = data => {
   const wall = $("#resources_found");
   wall.append(createResource(data));
 }
 
+// Currently nothing at /api/resources.
+// const loadResources = () => {
+//   $("#resources_found").empty();
+//   $.ajax({
+//     url: "/api/resources",
+//     method: "GET",
+//     dataType: "JSON",
+//   })
+//     .then(response => {
+//       renderResource(response);
+//     });
+// };
 
-const findAllResources = (input) => {
-  const queryString = `SELECT * FROM resources WHERE title LIKE $1;`;
-  const queryParams = [`%${input}%`];
-
-  pool.query(queryString, queryParams)
-    .then(res => {
-      console.log(res.rows)
-      return res.rows[0]
-    })
-    .catch(err => console.log(err))
-
-}
-
-findAllResources('Horoscope')
 
 $(document).ready(() => {
   renderResource('Horoscope')
