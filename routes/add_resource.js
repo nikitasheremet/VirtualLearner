135b9f57c50c7781df6cadc7c01dae9c9d35340e
Corah Goyette <corahgoyette@gmail.com>
2019-09-24 00:18:44 +0000

    add resource

--- routes/add_resource.js
+++ routes/add_resource.js
@@ -4,8 +4,20 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    res.render("register");
+    res.render("add_resource");
   })
+
+  // Route to show the new resource page
+  // router.post("/", (req, res) => {
+  //   let theirResource = req.session.theirResource;
+  //   if (theirResource === undefined) {
+  //     res.redirect("/home");
+  //   } else {
+  //     let templateVars = { resource: resources, user : db[theirResource]};
+  //     res.render("add_resource", templateVars);
+  //   }
+  // });
+
   return router;
 }
 
@@ -13,31 +25,20 @@ const bcrypt = require("bcrypt");
 
 
 
-$(document).ready(function() {
-  $('textarea').keyup(function() {
-    var remaining = 150 - jQuery(this).val().length;
-    jQuery('.counter').text(remaining);
-    if (remaining < 0){
-      jQuery('.counter').text("Error: Length exceeded " + remaining);
-      $(".counter").css({"color": "red"});
-    } else if (remaining >= 0){
-      jQuery('.counter').text(remaining);
-      // $(".counter").css({"color": ""});
-    }
-  });
-});
+// $(document).ready(function() {
+//   $('textarea').keyup(function() {
+//     var remaining = 150 - jQuery(this).val().length;
+//     jQuery('.counter').text(remaining);
+//     if (remaining < 0){
+//       jQuery('.counter').text("Error: Length exceeded " + remaining);
+//       $(".counter").css({"color": "red"});
+//     } else if (remaining >= 0){
+//       jQuery('.counter').text(remaining);
+//       // $(".counter").css({"color": ""});
+//     }
+//   });
+// });
 
 
-// Route to show the new resource page
-app.post("add_resource", (req, res) => {
-  let theirResource = req.session.theirResource;
-  if (theirResource === undefined) {
-    res.redirect("/home");
-  } else {
-    let templateVars = { resource: resources, user : db[theirResource]};
-    res.render("add_resource", templateVars);
-  }
-});
 
 
-//
