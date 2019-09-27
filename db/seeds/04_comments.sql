51098d31aba71710f023851e97dec14e3cb7f751
Corah Goyette <corahgoyette@gmail.com>
2019-09-27 02:12:09 +0000

    database

--- db/seeds/04_comments.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO comments (comment, user_id, resource_id) VALUES ('Every time I chop onion I can''t stop crying. Tips anyone??', 3, 4);
-INSERT INTO comments (comment, user_id, resource_id) VALUES ('Who here is also a Taurus?', 2, 2);
-INSERT INTO comments (comment, user_id, resource_id) VALUES ('^ Ski googles?', 1, 4);
